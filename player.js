f20d57a9e9c07f4b41d55213554c8379169cd4fb
kdowd <kdowd@freewheelmedia.net>
2020-03-05 13:20:39 -0500

    string

--- player.js
+++ player.js
@@ -164,7 +164,7 @@ tv.freewheel.DemoPlayer.prototype = {
 		$.each(videoElement, function(){ videoElement.controls = true; });
 		videoElement.src = contentSrc;
 		console.log("BEFORE SETTING CURRENT TIME: " + videoElement.currentTime + " CONTENT PAUSED ON: " + contentPausedOn);
-		videoElement.currentTime = 10;
+		videoElement.currentTime = "10.00";
 		console.log("AFTER SETTING CURRENT TIME: " + videoElement.currentTime + " CONTENT PAUSED ON: " + contentPausedOn);
 		console.log("===========resume video after: " + contentPausedOn);
 		videoElement.addEventListener('ended', this.onContentVideoEnded.bind(this));
